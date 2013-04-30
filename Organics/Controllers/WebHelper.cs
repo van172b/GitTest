a4b177af7b67d5133aaacc35496018020a05f7cd
jvanecko <jvanecko@gmail.com>
2013-04-29 21:54:53 -0500

    ckeditor shown but does not send data to server

---
+++ Organics/Controllers/WebHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Linq.Expressions;
+
+
+namespace Organics.Common
+{
+    public static class HtmlExtensions
+    {
+        public static MvcHtmlString ClientIdFor<TModel, TProperty>(
+            this HtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, TProperty>> expression)
+        {
+            return MvcHtmlString.Create(
+                  htmlHelper.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldId(
+                      ExpressionHelper.GetExpressionText(expression)));
+        }
+    }
+}
